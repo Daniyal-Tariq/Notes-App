938409cc990f04160513e279c11c3dbbfa08663e
Daniyal Tariq <dronzerdroid@gmail.com>
2021-02-05 19:48:44 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,73 @@
+const notes = require('./notes.js')
+const yargs = require('yargs')
+const chalk = require('chalk')
+
+// Customize yargs version
+yargs.version('1.1.0')
+
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+       notes.addNote(argv.title, argv.body)
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+         notes.removeNote(argv.title)
+    //    console.log('im working')
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List all the notes',
+    handler() {
+        notes.listNotes()
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read the note',
+    builder: {
+        title: {
+            describe: 'Note Title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.readNote(argv.title)
+    }
+})
+
+//add, remove, read, list
+
+yargs.parse()
